687008326b1d84cfee3ae99c975fcb41975e0300
NicolasJamar <nicolas.jamar@gmail.com>
2018-02-13 10:58:31 +0100

    form ajouter

--- formulaire.php
+++ formulaire.php
@@ -1,4 +1,17 @@
-<?php
+<h2>Ajouter une tâche</h2>
+<form action="todo.json" method="post" >
+	<input type="text" name="tache">
+	<input type="submit" value="ajouter">
+</form>
 
+
+<?php
+	if(isset($_POST['tache'])){
+		$tache = $_POST['tache'];
+		$todo = 'todo.json';
+
+		file_put_contents($todo , $tache, FILE_APPEND);
+
+	}
 
 ?>
