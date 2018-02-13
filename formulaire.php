6b247b5dcc86b59351371b00859c5c2adf62fad5
NicolasJamar <nicolas.jamar@gmail.com>
2018-02-13 12:23:40 +0100

    array s efface a chaque fois

--- formulaire.php
+++ formulaire.php
@@ -1,16 +1,25 @@
 <h2>Ajouter une tâche</h2>
-<form action="todo.json" method="post" >
+<form action="formulaire.php" method="post" >
 	<input type="text" name="tache">
 	<input type="submit" value="ajouter">
 </form>
 
 
 <?php
-	if(isset($_POST['tache'])){
-		$tache = $_POST['tache'];
-		$todo = 'todo.json';
+		$afaire = array();
+		$archives = array();
 
-		file_put_contents($todo , $tache, FILE_APPEND);
+	if(isset($_POST['tache'])){
+
+
+		array_push($afaire, $_POST['tache'] );
+		print_r($afaire);
+		$todo = json_encode($afaire);
+
+		// $tache = $_POST['tache'];
+		// $todo = 'todo.json';
+
+		// file_put_contents($todo , $tache, FILE_APPEND);
 
 	}
 
