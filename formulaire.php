06f613784ab59c5be0b9b700654d18afeae2b2d8
NicolasJamar <nicolas.jamar@gmail.com>
2018-02-14 14:35:34 +0100

    solution Glenn

--- formulaire.php
+++ formulaire.php
@@ -1,99 +1,89 @@
 <?php
-	// if(isset($_POST['tache'])){
+	if(isset($_POST['tache'])){
 
-	// 	$todo = file_get_contents('todo.json');
+		$todo = file_get_contents('todo.json');
+		$decodeTodo = json_decode($todo, true);
 
-	// 	$todo = json_decode($todo);
+		var_dump($decodeTodo);
 
-		// if(empty($todo)){
-		// 	$todo = array();
-		// } 
-
-		// $i = count($todo); // count() = au .length de JS
-		// $todo[$i] = $_POST['tache'];
-		// var_dump($todo);
-
-		// $todo["afaire"][] = $_POST['tache'];
-
+		$decodeTodo['aFaire'][] = htmlspecialchars($_POST['tache']);
 		
-		
-		// $todo = json_encode($todo, JSON_FORCE_OBJECT);
+		$newTodo = json_encode($decodeTodo, JSON_FORCE_OBJECT);
 
-		// file_put_contents('todo.json', $todo);
+		file_put_contents('todo.json', $newTodo);
 
-		
-
-		
-		// for ($i=0; $i < count($todo); $i++) { 
-		// 	# code...
-		// }
-
-		// file_put_contents($todolist, $afaire, FILE_APPEND | LOCK_EX);
-
-	// }
-
+	}
 ?>
 
 
 <?php
 //Si l'input nouvelle tâche existe et n'est pas vide
-if (ISSET($_POST["tache"]) && !EMPTY($_POST["tache"]))
-{
-// Import les données du fichier
-$file = file_get_contents('todo.json');
-// Si le fichier est vide, créer un objet vide $taches et un tableau vide $aFaire
-if (EMPTY($file))
-{
-$taches = new stdClass();
-$aFaire = array();
-}
-// Sinon, traduction du fichier en objet php dans la variable $taches et copie du tableau de l'objet dans $aFaire
-else
-{
-$taches = json_decode($file);
-$aFaire = $taches->aFaire;
-}
-// Copie de l'input nouvelle tâche
-$newTache = $_POST["tache"];
-// Nettoyage du fichier pour éviter les injections de base
-$newTache = htmlspecialchars($newTache);
-// Ajout de la nouvelle tâche dans le tableau
-$aFaireLength = count($aFaire);
-$aFaire[$aFaireLength] = $newTache;
-// Ecraser l'ancienne la propriété tableau aFaire avec le tableau $aFaire mis à jour
-$taches->aFaire = $aFaire;
-// Traduction au format Json
-$taches = json_encode($taches);
-// Ecraser le fichier avec les nouvelles données
-file_put_contents('todo.json', $taches);
-// AMEN
-}
+// if (ISSET($_POST["tache"]) && !EMPTY($_POST["tache"]))
+// {
+// // Import les données du fichier
+// $file = file_get_contents('todo.json');
+// // Si le fichier est vide, créer un objet vide $taches et un tableau vide $aFaire
+// if (EMPTY($file))
+// {
+// $taches = new stdClass();
+// $aFaire = array();
+// }
+// // Sinon, traduction du fichier en objet php dans la variable $taches et copie du tableau de l'objet dans $aFaire
+// else
+// {
+// $taches = json_decode($file);
+// $aFaire = $taches->aFaire;
+// }
+// // Copie de l'input nouvelle tâche
+// $newTache = $_POST["tache"];
+// // Nettoyage du fichier pour éviter les injections de base
+// $newTache = htmlspecialchars($newTache);
+// // Ajout de la nouvelle tâche dans le tableau
+// $aFaireLength = count($aFaire);
+// $aFaire[$aFaireLength] = $newTache;
+// // Ecraser l'ancienne la propriété tableau aFaire avec le tableau $aFaire mis à jour
+// $taches->aFaire = $aFaire;
+// // Traduction au format Json
+// $taches = json_encode($taches);
+// // Ecraser le fichier avec les nouvelles données
+// file_put_contents('todo.json', $taches);
+// // AMEN
+// }
 ?>
 
-
-<h2>A Faire</h2>
-<form action="" method="post">
-	<input type="checkbox" name="checkbox">
-	<?php 
-
-	?>
-	<input type="submit" value="Enregistrer">
-</form>
-
-<h2>Archive</h2>
-<form action="" method="post">
-	<input type="checkbox" name="checkbox">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
 	
-</form>
+
+	<h2>A Faire</h2>
+	<form action="" method="post">
+		<input type="checkbox" name="checkbox">
+		<?php 
+
+		?>
+		<input type="submit" value="Enregistrer">
+	</form>
+
+	<h2>Archive</h2>
+	<form action="" method="post">
+		<input type="checkbox" name="checkbox">
+		
+	</form>
 
 
 
-<h2>Ajouter une tâche</h2>
-<form action="" method="post" >
-	<input type="text" name="tache">
-	<input type="submit" value="ajouter">
-</form>
+	<h2>Ajouter une tâche</h2>
+	<form action="" method="post" >
+		<input type="text" name="tache">
+		<input type="submit" value="ajouter">
+	</form>
 
-
+</body>
+</html>
 
 
